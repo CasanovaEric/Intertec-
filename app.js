0279eab40d0b77e3672a47859a0e0ec9e6222625
rolando <rolandoduarte83@gmail.com>
2021-11-20 21:33:41 -0300

    vinculando al repo

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// const express = require('express');
-// const app = express();
-// app.use(express.static('public'));
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(__dirname + '/views/home.html');
-// });
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
-// // app.get('/login', (req,res)=>{
-// //     res.sendFile(__dirname + '/views/login.html');
-// // });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
 
-// // app.get('/register', (req,res)=>{
-// //     res.sendFile(__dirname + '/views/register.html');
-// // });
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
 
 
 app.listen(3000, ()=>{
