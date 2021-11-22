6f774d8422ac6ef3e4efabb4ec485565276a8980
Rolando <rolandoduarte83@gmail.com>
2021-11-22 17:20:12 -0300

    arreglos del proyectos

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+// const express= require('express');
+// const path = require('path')
+// const app = express();
+// app.use(express.static('public'));
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req,res)=> {
+//     res.sendFile(path.resolve(__dirname + '/views/home.html'))
+// })
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/home.html');
+// });
+
+// app.get('/login', (req,res)=>{
+//     res.sendFile(__dirname + '/views/login.html');
+// });
+
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-
-
-app.listen(3000, ()=>{
+// app.listen(3000, ()=>{
     console.log('Servidor funcionando');
-});
+// }); 
