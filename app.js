81c3484125e57db479ea32c481438a9d8a4fc743
Rolando <rolandoduarte83@gmail.com>
2021-11-24 21:27:52 -0300

    armado de la pagina samsung

--- app.js
+++ app.js
@@ -13,15 +13,52 @@
 //     res.sendFile(__dirname + '/views/home.html');
 // });
 
-// app.get('/login', (req,res)=>{
-//     res.sendFile(__dirname + '/views/login.html');
-// });
+// // app.get('/login', (req,res)=>{
+// //     res.sendFile(__dirname + '/views/login.html');
+// // });
 
-// app.get('/register', (req,res)=>{
-//     res.sendFile(__dirname + '/views/register.html');
-// });
+// // app.get('/register', (req,res)=>{
+// //     res.sendFile(__dirname + '/views/register.html');
+// // });
 
 
 // app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+//     console.log('Servidor funcionando');
 // }); 
+
+
+
+
+
+
+
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.listen(process.env.PORT || 3000, function() {
+    
+    console.log('Servidor corriendo en puerto 3000');
+});
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/registro.html');
+});
+
+
+app.post('/', (req,res)=>{
+    console.log(req.body);
+    res.sendFile(__dirname + '/views/login.html');
+});
