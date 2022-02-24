f774f396f6cbdfb8410ad379541b82164120c415
Eric Casanova <casanovaeric95@gmail.com>
2022-02-23 22:06:07 -0300

    actualizando rama

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -68,3 +68,10 @@ const controller= {
 
 //"MODULE EXPORT"
 module.exports= controller;
+
+var ds= 36
+var fe=3
+
+if (ds/fe >12) {
+    console.log("puedo divirlo")
+} else { console.log("no puedo") }
